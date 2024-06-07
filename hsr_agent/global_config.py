1d383e258a99d76565e102659a1e4e82d5253c98
CreativeAI2030 <CreativeAI2030@gmail.com>
2024-06-08 03:32:48 +0900

    0608

--- hsr_agent/global_config.py
+++ hsr_agent/global_config.py
@@ -12,8 +12,8 @@ PC_TOPIC = '/hsrb/head_rgbd_sensor/depth_registered/rectified_points'
 
 # 기존 경로 : 'weight/best_240409.pt'
 # YOLO weight 변경 시 경로 변경
-yolo_weight_path = 'weight/best_0704.pt'
-yolo_classnames_path = 'weight/test.cn'
+yolo_weight_path = 'weight/best_for_final_not_tiny.pt'
+yolo_classnames_path = 'weight/test_0608.cn'
 
 OBJECT_LIST = make_object_list(yolo_classnames_path)
 
