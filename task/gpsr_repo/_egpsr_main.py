e1898ca0e3d9bd70bf022c7ab006d2463c083b3d
CreativeAI2030 <CreativeAI2030@gmail.com>
2024-06-09 13:33:42 +0900

    egpsr initial commit

--- task/gpsr_repo/_egpsr_main.py
+++ task/gpsr_repo/_egpsr_main.py
@@ -3,16 +3,22 @@ sys.path.append("task/gpsr_repo/")
 from gpsr_config import *
 
 from _gpsr_main import GPSR
+from itertools import cycle
 
 
 class EGPSR(GPSR):
     def __init__(self, agent):
         super().__init__(agent)
-        self.cur_room = 'living room'
         self.mov_fin = True
+        self.room_cycle = cycle(self.rooms_list)
 
 def egpsr(agent):
     eg = EGPSR(agent)
     
-    while eg.task_finished_count < task_iteration:
-        
+    for cur_room in eg.room_cycle:
+        if eg.task_finished_count >= task_iteration:
+            break
+
+        eg.move(cur_room)
+
+        eg.identify()
