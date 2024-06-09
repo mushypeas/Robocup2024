7c66ac3f8d6c2f4c35269d9bf208f59cc4ad2223
jinupahk2001 <parkwls3114@naver.com>
2024-06-09 14:40:43 +0900

    tmp

--- module/person_following_bot/follow.py
+++ module/person_following_bot/follow.py
@@ -55,15 +55,27 @@ def get_iou(boxA, boxB):
 	return iou
 
 def new_get_controls(x,z):
-	linear_max = 0.5#0.8
-	angular_max = 0.5 #0.5
-	twist = Twist()
+	if z > 1.2: 
+		linear_max = 2.0#0.8
+		angular_max = 0.5 #0.5
+		twist = Twist()
 
-	# linear = (z/1000-0.6)/3
-	# linear = z/1000*0.4
-	angular = (-1/500) * (x-320)
-	# print('linear: {} ,angular: {}  \n'.format(linear,angular))
-	linear = z / 1000 * .8
+		# linear = (z/1000-0.6)/3
+		# linear = z/1000*0.4
+		angular = (-1/500) * (x-320)
+		# print('linear: {} ,angular: {}  \n'.format(linear,angular))
+		linear = z / 1000 * .4
+	else:
+		linear_max = 0.5#0.8
+		angular_max = 0.5 #0.5
+		twist = Twist()
+
+		# linear = (z/1000-0.6)/3
+		# linear = z/1000*0.4
+		angular = (-1/500) * (x-320)
+		# print('linear: {} ,angular: {}  \n'.format(linear,angular))
+		linear = z / 1000 * .8
+
 
 	# if linear > 0.8:
 	# 	linear *= 0.8
