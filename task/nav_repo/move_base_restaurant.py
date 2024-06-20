2f98ccea9e9ad963d43ce4edd6e4f7c45cc787cf
CreativeAI2030 <CreativeAI2030@gmail.com>
2024-06-20 19:58:26 +0900

    feat: move_rel

--- task/nav_repo/move_base_restaurant.py
+++ task/nav_repo/move_base_restaurant.py
@@ -12,31 +12,42 @@ class MoveBaseRestaurant:
         self.base_action_client.wait_for_server(timeout=2)
         self.initial_pose_pub = rospy.Publisher('/laser_2d_correct_pose', PoseWithCovarianceStamped, queue_size=10)
         
+    def get_pose(self):
+        while not rospy.is_shutdown():
+            try:
+                (trans, rot) = self.listener.lookupTransform('map', 'base_footprint', rospy.Time(0))
+                break
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue
+
+        euler_angles = tf.transformations.euler_from_quaternion(rot)
+        yaw = euler_angles[2]
+
+        return [trans[0], trans[1], yaw]
+    
     def move_rel(self, x, y, yaw=0, wait=False):
-        self.base_action_client.wait_for_server()
+        self_x, self_y, self_yaw = self.get_pose()
+        goal_x, goal_y, goal_yaw = x + self_x, y + self_y, yaw + self_yaw
+        self.base_action_client.wait_for_server(timeout=2)
 
         pose = PoseStamped()
         pose.header.stamp = rospy.Time.now()
-        pose.header.frame_id = "base_link"
-        pose.pose.position = Point(x, y, 0)
-        quat = tf.transformations.quaternion_from_euler(0, 0, yaw)
+        pose.header.frame_id = "map"
+        pose.pose.position = Point(goal_x, goal_y, 0)
+        quat = tf.transformations.quaternion_from_euler(0, 0, goal_yaw)
         pose.pose.orientation = Quaternion(*quat)
 
         goal = MoveBaseGoal()
         goal.target_pose = pose
         # send message to the action server
         self.base_action_client.send_goal(goal)
-        # wait for the action server to complete the order
         if wait:
+            # wait for the action server to complete the order
             self.base_action_client.wait_for_result()
             # print result of navigation
             action_state = self.base_action_client.get_state()
-            print("action state", action_state)
             if action_state == GoalStatus.SUCCEEDED:
                 rospy.loginfo("Navigation Succeeded.")
-                return True
             else:
-                rospy.loginfo("Navigation FAILED!!!!")
+                rospy.logerr("Navigation FAILED!!!!")
                 return False
-
-        return True
