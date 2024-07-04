394a9a9cd25612eeaefcf271e2818617d8210b3f
jinupahk2001 <parkwls3114@naver.com>
2024-07-04 21:03:33 +0900

    240704 inspection

--- task/inspection.py
+++ task/inspection.py
@@ -6,5 +6,5 @@ def inspection(agent):
     agent.say('start inspection')
     agent.door_open()
     agent.move_rel(1.0, 0, wait=True)
-    agent.move_abs_safe('insp_target', thresh=0.45, timeout=5, giveup_timeout=10000, angle=90)
+    agent.move_abs_safe('insp_target', thresh=0.75, timeout=10, giveup_timeout=10000, angle=45)
     agent.say('navigation succeeded.')
