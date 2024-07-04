ada64fb7e51a9a645cdad41148d3853bd42e10df
jinupahk2001 <parkwls3114@naver.com>
2024-07-04 17:26:57 +0900

    240704 restaurant_escape_from_stuck

--- task/restaurant.py
+++ task/restaurant.py
@@ -18,6 +18,35 @@ from playsound import playsound
 from std_srvs.srv import Trigger
 
 
+
+min_interval_arc_len = 1.0
+unit_rad = 0.25 * ( math.pi / 180 )
+avg_dist_move_dist_ratio = 5
+
+def calculate_human_rad(human_center_x, yolo_img_width):
+    human_center_bias = human_center_x - yolo_img_width / 2
+    return -human_center_bias / 640
+
+def index_to_rad(idx):
+    return (idx - center_index) * unit_rad
+
+################ Maybe Constant? ################
+
+## main frequency
+main_freq = 1
+main_period = 1.0 / main_freq
+
+## LiDAR dist
+min_dist = 1.0
+max_dist = 5.0
+
+## LiDAR index
+lidar_index = 963
+center_index = lidar_index // 2
+
+## YOLO img size
+yolo_img_height = 480
+yolo_img_width = 640
 '''
 
 foo@bar:~/robocup2024/module/waver_detector$> python run_openpose.py
@@ -34,7 +63,6 @@ class MoveBaseStandalone:
         # jnpahk
         self.lidar_sub = rospy.Subscriber('/hsrb/base_scan', LaserScan, self._lidar_callback)
         
-        
         # reset map
         # rospy.wait_for_service('/reset_map')
 
@@ -45,6 +73,70 @@ class MoveBaseStandalone:
         self.dists = data_np
         self.indices_in_range = np.where(self.dists > min_dist)[0].tolist()
         self.candidates = self.find_candidates()
+
+
+
+    def find_candidates(self):
+        indices_in_range = self.indices_in_range
+
+        if not indices_in_range:
+            return []
+
+        segments = []
+        start_idx = indices_in_range[0]
+        end_idx = indices_in_range[0]
+
+        for i in range(1, len(indices_in_range)):
+            if indices_in_range[i] == indices_in_range[i - 1] + 1:
+                end_idx = indices_in_range[i]
+                
+            else:
+                start_rad = index_to_rad(start_idx)
+                end_rad = index_to_rad(end_idx)
+                min_dist = min(self.dists[start_idx:end_idx + 1])
+        
+                interval_arc_len = (end_rad - start_rad) * min_dist
+
+                if interval_arc_len > min_interval_arc_len:
+                    number_seg = int(interval_arc_len / min_interval_arc_len)
+                    idx_len = end_idx - start_idx
+
+                    for j in range(number_seg):
+                        new_start_idx = start_idx + int(idx_len * j / number_seg)
+                        new_end_idx = start_idx + int(idx_len * (j + 1) / number_seg)
+                        new_min_dist = min(self.dists[new_start_idx:new_end_idx + 1])
+                        new_start_rad = index_to_rad(new_start_idx)
+                        new_end_rad = index_to_rad(new_end_idx)
+
+                        segments.append([new_start_rad, new_end_rad, new_min_dist])
+
+                start_idx = indices_in_range[i]
+                end_idx = indices_in_range[i]
+
+        start_rad = index_to_rad(start_idx)
+        end_rad = index_to_rad(end_idx)
+        min_dist = min(self.dists[start_idx:end_idx + 1])
+
+        interval_arc_len = (end_rad - start_rad) * min_dist
+
+        if interval_arc_len > min_interval_arc_len:
+            number_seg = int(interval_arc_len / min_interval_arc_len)
+            idx_len = end_idx - start_idx
+
+            for j in range(number_seg):
+                new_start_idx = start_idx + int(idx_len * j / number_seg)
+                new_end_idx = start_idx + int(idx_len * (j + 1) / number_seg)
+                new_min_dist = min(self.dists[new_start_idx:new_end_idx + 1])
+                
+                new_start_rad = index_to_rad(new_start_idx)
+                new_end_rad = index_to_rad(new_end_idx)
+
+                segments.append([new_start_rad, new_end_rad, new_min_dist])
+
+        return segments
+
+
+    
 
     def move_zero(self, agent):
         self.base_action_client.wait_for_server(timeout=2)
@@ -72,11 +164,13 @@ class MoveBaseStandalone:
                pass
 
     def move_customer(self, agent, goal_x, goal_y, goal_yaw=None):
+        
         self.base_action_client.wait_for_server(5)
         theta = 0.
         rotate_delta = 30.
         r = 1.0 # 0.5
         spin_count = 0
+
         while not rospy.is_shutdown():
             # goal topic generation
             _goal_x = goal_x - r * math.cos(math.pi * (theta / 180))
@@ -91,10 +185,13 @@ class MoveBaseStandalone:
             pose.pose.orientation = Quaternion(*quat)
             goal = MoveBaseGoal()
             goal.target_pose = pose
+
             # send message to the action server
             self.base_action_client.send_goal(goal)
+            
             # Retry navigation to the customer until it success
             while not rospy.is_shutdown():
+
                 rospy.sleep(0.1)
                 action_state = self.base_action_client.get_state()
                 if action_state == GoalStatus.SUCCEEDED:
@@ -116,7 +213,20 @@ class MoveBaseStandalone:
                         r += 0.1
                     break
                 else:
+                    if (time.time() - self.agent.last_moved_time) > 7:
+                        try:
+                            candidates = self.candidates
+                        except AttributeError:
+                            continue
+                        best_interval = self.get_best_candidate(candidates)
+                        while not best_interval:
+                            best_interval = self.get_best_candidate(candidates)
+                        self.move_best_interval(best_interval)
                     pass
+
+    def move_rel(self, x, y, yaw=0):
+        self.agent.move_rel(x, y, yaw=yaw)
+
 
     def move_abs(self, agent, goal_x, goal_y, goal_yaw=None):
         self.base_action_client.wait_for_server(5)
@@ -145,6 +255,42 @@ class MoveBaseStandalone:
                     return False
                 else:
                     pass
+
+
+    
+    def heuristic(self, start_rad, end_rad, avg_dist):
+        avg_rad = (start_rad + end_rad) / 2
+        
+        # if self.human_rad:
+        return -abs(avg_rad - 481) #current center
+        
+        # else:
+        #     ### TODO: heuristic function when human_yaw is None ###
+        #     ### Momentum + past human_yaw ###
+        #     return 0
+
+    def get_best_candidate(self, candidates):
+        heuristic_values = [self.heuristic(start, end, avg_dist) for start, end, avg_dist in candidates]
+        # if not heuristic_values:
+        #     return None
+        best_idx = np.argmax(heuristic_values)
+        return candidates[best_idx]
+    
+    def move_best_interval(self, interval):
+        start_rad = interval[0]
+        end_rad = interval[1]
+        avg_dist = interval[2]
+        
+        move_dist = avg_dist / avg_dist_move_dist_ratio
+        avg_rad = (start_rad + end_rad) / 2
+        
+        move_x = move_dist * math.cos(avg_rad)
+        move_y = move_dist * math.sin(avg_rad)
+        # if self.human_rad:
+        #     move_yaw = self.human_rad
+        # else:
+        #     move_yaw = 0
+        self.move_rel(move_x, move_y, yaw=0)
 
 
 def nav_target_from_pc(pc, table, robot_ori, K=100):
