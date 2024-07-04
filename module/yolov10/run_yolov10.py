5fdcc4970d59916e5636e4c3139198f1a3d1eadb
younjin <yjinzero@gmail.com>
2024-07-04 15:08:32 +0900

    YOLOV10

--- module/yolov10/run_yolov10.py
+++ module/yolov10/run_yolov10.py
@@ -5,7 +5,7 @@ import math
 import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
-from ultralytics import Yolov10
+from ultralytics import YOLOv10
 import numpy as np
 import supervision as sv
 
@@ -15,7 +15,6 @@ import ros_numpy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image, PointCloud2
 from std_msgs.msg import Int16MultiArray, Float32MultiArray
-import time
 
 import sys
 sys.path.append('../../../robocup2024')
@@ -30,8 +29,7 @@ class custom_Yolov10:
         self.yolo_bbox = []
         self.bridge = CvBridge()
 
-        rgb_topic = '/hsrb/head_rgbd_sensor/rgb/image_rect_color'
-        self._rgb_sub = rospy.Subscriber(rgb_topic, Image, self._rgb_callback)
+        self._rgb_sub = rospy.Subscriber(RGB_TOPIC, Image, self._rgb_callback)
         self._pc_sub = rospy.Subscriber(PC_TOPIC, PointCloud2, self._pc_callback, queue_size=1)
         self.yolo_pub = rospy.Publisher('/snu/yolo', Int16MultiArray, queue_size=10)
 
@@ -47,7 +45,7 @@ class custom_Yolov10:
         self.pc = ros_numpy.numpify(point_msg)
 
     def ready(self):
-        self.model = Yolov10("best.pt")  #path for weight file
+        self.model = YOLOv10("best.pt")  #path for weight file
         self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in OBJECT_LIST]
 
     def detect(self):
@@ -80,13 +78,19 @@ class custom_Yolov10:
                 width = c2[0] - c1[0]
                 height = c2[1] - c1[1]
 
+                tl = 1
+                color =colors[int(cls)] 
+
                 bbox_list.append([cent_x, cent_y, width, height, int(cls)])
                 bbox_with_conf_list.append([cent_x, cent_y, width, height, int(cls), int(conf * 100)]) #conf: bbox score
 
-                cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), colors[int(cls)], 2)
-                cv2.putText(img, label, (int(box[0]), int(box[1] - 10)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
-      
+                cv2.rectangle(img, c1, c2, color, Thickness = tl, lineType = cv2.LINE_AA)
+                
+                tf = max(tl - 1, 1)  # font thickness
+                t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+                c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+                cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+                cv2.putText(img, label,(c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
         # show result
         view_img = True
         if view_img:
@@ -108,22 +112,9 @@ class custom_Yolov10:
         yolo_img_msg = self.bridge.cv2_to_imgmsg(img, encoding='bgr8')
         self.yolo_img_pub.publish(yolo_img_msg)
 
-def get_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='weight/best_0704.pt', help='model.pt path(s)')
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    opt = parser.parse_args()
-    return opt
 
 if __name__ == '__main__':
-    opt = get_opt()
-    print(opt)
+
     rospy.init_node('hsr_yolov10', anonymous=True)
     yolov10_controller = custom_Yolov10()
     image_resolution = (480, 640, 3)
@@ -134,10 +125,7 @@ if __name__ == '__main__':
             if bbox_list is None or yolov10_controller.pc is None:
                 continue
 
-            # _pc = yolov7_controller.pc.reshape(480, 640)
-            # pc_np = np.array(_pc.tolist())[:, :, :3]
             yolov10_controller.yolo_publish(bbox_list)
-            yolov10_controller.yolo_with_conf_publish(bag_bbox_list)
-            # print(img)    
+            yolov10_controller.yolo_with_conf_publish(bag_bbox_list)    
             yolov10_controller.yolo_img_publish(img)
             r.sleep()
