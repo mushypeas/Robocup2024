77f815fef0e91da93dfd04a7689c404a1fa96f78
CreativeAI2030 <CreativeAI2030@gmail.com>
2024-07-05 18:03:27 +0900

    feat: gpsr_model_class

--- task/gpsr_repo/_gpsr_main.py
+++ task/gpsr_repo/_gpsr_main.py
@@ -19,38 +19,9 @@ from geometry_msgs.msg import Twist
 
 import torch
 import numpy as np
-import torch.nn as nn
 
 objects_data = readData(objects_file_path)
 
-class PoseClassifier(nn.Module):
-    def __init__(self, input_size):
-        super(PoseClassifier, self).__init__()
-        self.fc1 = nn.Linear(input_size, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 3)
-        self.relu = nn.ReLU()
-    
-    def forward(self, x):
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-    
-class GestClassifier(nn.Module):
-    def __init__(self, input_size):
-        super(GestClassifier, self).__init__()
-        self.fc1 = nn.Linear(input_size, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 4)  # 4개의 클래스
-        self.relu = nn.ReLU()
-    
-    def forward(self, x):
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-    
 class GPSR:
     def __init__(self, agent):
         self.agent = agent
