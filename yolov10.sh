0fd1cef0a5731b4dfad15e02670995257ec2745f
mushypeas <qkrwoans2@gmail.com>
2024-07-08 09:26:27 +0900

    Qweqwe

--- yolov10.sh
+++ yolov10.sh
@@ -1,5 +1,6 @@
 cd ~/Robocup2024;
-# source ./venv/bin/activate;
-yolov10
+conda init;
+export ROS_MASTER_URI=http://hsrb.local:11311 export PS1="\[\033[41;1;37m\]<hsrb>\[\033[0m\]\w$ ";
+conda activate yolov10;
 cd /home/tidy/Robocup2024/module/yolov10;
 python run_yolov10.py;
