9f0324d3dcf065a84ef152cb1a8c56abf24f0b54
mushypeas <qkrwoans2@gmail.com>
2024-07-08 09:09:21 +0900

    sh

---
+++ yolov10.sh
@@ -0,0 +1,5 @@
+cd ~/Robocup2024;
+# source ./venv/bin/activate;
+yolov10
+cd /home/tidy/Robocup2024/module/yolov10;
+python run_yolov10.py;
---
+++ yolov7_pose.sh
@@ -0,0 +1,4 @@
+cd ~/Robocup2024;
+rb
+cd /home/tidy/Robocup2024/module/yolov7-pose-estimation;
+python pose-estimate-ros.py;
