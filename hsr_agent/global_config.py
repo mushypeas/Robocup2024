77b99752075d7614ef627fd90cdb4fe6ce0a4e29
mushypeas <qkrwoans2@gmail.com>
2024-07-09 00:52:02 +0900

    aa

--- hsr_agent/global_config.py
+++ hsr_agent/global_config.py
@@ -11,12 +11,17 @@ RGB_TOPIC = '/hsrb/head_rgbd_sensor/rgb/image_rect_color'
 DEPTH_TOPIC = '/hsrb/head_rgbd_sensor/depth_registered/image_rect_raw'
 PC_TOPIC = '/hsrb/head_rgbd_sensor/depth_registered/rectified_points'
 
-# 기존 경로 : 'weight/best_240409.pt'
-# YOLO weight 변경 시 경로 변경
+
+##### yolo v7########
 # yolo_weight_path = 'weight/best_0704.pt'
-yolo_weight_path = 'weight/best_for_final_not_tiny.pt'
+# yolo_weight_path = 'weight/best_for_final_not_tiny.pt'
 # yolo_classnames_path = 'weight/best_for_final.cn'
+###################
+
+##### yolo v10########
+yolo_weight_path = 'weight/best_0704.pt'
 yolo_classnames_path = 'weight/YOLOV10-M-SNU-0703.cn'
+###################
 
 try:
     OBJECT_LIST = make_object_list(yolo_classnames_path)
