6d91cc1b5625c8ac078091f4450589077802acaf
mushypeas <qkrwoans2@gmail.com>
2024-07-10 11:58:03 +0900

    restore

--- task/restaurant.py
+++ task/restaurant.py
@@ -492,8 +492,8 @@ def restaurant(agent):
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     roslaunch.configure_logging(uuid)
 
-    head_map_client = dynamic_reconfigure.client.Client("/tmc_map_merger/inputs/head_rgbd_sensor")
-    head_map_client.update_configuration({"enable": True})
+    # head_map_client = dynamic_reconfigure.client.Client("/tmc_map_merger/inputs/head_rgbd_sensor")
+    # head_map_client.update_configuration({"enable": True})
 
     slam_args = ['tidyboy_nav_stack', 'hector.launch']
     nav_args  = ['tidyboy_nav_stack', 'nav_stack.launch']
